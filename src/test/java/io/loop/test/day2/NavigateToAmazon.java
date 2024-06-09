daf46cefa5e3a06e26085c0ba9ff0a4940453edc
Alex Cheble <45234022+Alexcheble@users.noreply.github.com>
2024-06-09 15:00:21 -0400

    day3 commit

---
+++ src/test/java/io/loop/test/day2/NavigateToAmazon.java
@@ -0,0 +1,31 @@
+package io.loop.test.day2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+   navigate to amazon
+   maximize
+   navigate back
+   navigate forward
+   refresh
+    */
+public class NavigateToAmazon {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+
+
+        driver.get("https://www.amazon.com/ref=nav_logo");
+
+        driver.manage().window().maximize();
+
+        driver.navigate().back();
+
+        driver.navigate().forward();
+
+        driver.navigate().refresh();
+
+    }
+}
---
+++ src/test/java/io/loop/test/day3/T1_locators_getText.java
@@ -0,0 +1,49 @@
+package io.loop.test.day3;
+
+import io.loop.test.utilities.DocuporConstants;
+import io.loop.test.utilities.WebDriverUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T1_locators_getText {
+    /*
+     * go to docuport
+     * enter username
+     * do NOT enter password
+     * click on login button
+     * verify error message - please enter password
+     */
+
+//    Logins
+//
+//Client:      b1g2_client@gmail.com    Group2
+//Supervisor:      b1g2_supervisor@gmail.com    Group2
+//Advisor:      b1g2_advisor@gmail.com    Group2
+//Employee:      b1g2_employee@gmail.com    Group2
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://beta.docuport.app");
+
+        WebElement userName = driver.findElement(By.id("input-14"));
+        userName.sendKeys(DocuporConstants.USERNAME_CLIENT);
+
+        WebElement loginButton = driver.findElement(By.className("v-btn__content"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.className("v-messages__message"));
+        String actualErrorMessage = errorMessage.getText();
+
+        if(actualErrorMessage.equals(DocuporConstants.ERROR_MESSAGE_FOR_EMPTY_PASSWORD)) {
+            System.out.println("Expected error message: " + DocuporConstants.ERROR_MESSAGE_FOR_EMPTY_PASSWORD + ", matches actual error message: " + actualErrorMessage + " => TEST PASS");
+        } else {
+            System.err.println("Expected error message: " + DocuporConstants.ERROR_MESSAGE_FOR_EMPTY_PASSWORD + ", DOESNT MATCH actual error message: " + actualErrorMessage + " => TEST FAIL");
+        }
+
+        driver.quit();
+
+    }
+
+}
