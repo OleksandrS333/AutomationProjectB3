9943d8e6deab31da2b4ccead93d8fe7de3bc2476
Alex Cheble <45234022+Alexcheble@users.noreply.github.com>
2024-06-16 10:10:44 -0400

    commit_day4

--- src/test/java/io/loop/test/utilities/DocuporConstants.java
+++ src/test/java/io/loop/test/utilities/DocuporConstants.java
@@ -1,7 +1,10 @@
 package io.loop.test.utilities;
 
 public class DocuporConstants {
-    public static final String USERNAME_CLIENT = "b1g2_client@gmail.com";
+    public static String USERNAME_CLIENT = "b1g2_client@gmail.com";
     public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "Please enter your password";
 
+//    public static String getUSERNAME_CLIENT() {
+//        return USERNAME_CLIENT;
+//    }
 }
