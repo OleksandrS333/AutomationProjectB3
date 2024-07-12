00e515376e460e5b33ad353b919ae56e83921114
Alex Cheble <45234022+Alexcheble@users.noreply.github.com>
2024-07-11 22:00:26 -0400

    faker, configuration properties, configuration reader, Driver, upload

--- src/test/java/io/loop/test/base/TestBase.java
+++ src/test/java/io/loop/test/base/TestBase.java
@@ -1,22 +1,25 @@
 package io.loop.test.base;
 
+import io.loop.test.utilities.ConfigurationReader;
 import io.loop.test.utilities.WebDriverUtil;
-import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
+
+
     public WebDriver driver;
 
     @BeforeMethod
     public void setUpMethod(){
-        driver = WebDriverUtil.getDriver("chrome");
+        driver = WebDriverUtil.getDriver(ConfigurationReader.getProperties("browser"));
         driver.manage().window().maximize();
     }
-//    @AfterMethod
-//    public void tearDownMethod(){
-//        driver.close();
-//    }
+
+    //@AfterMethod
+    public void tearDownMethod(){
+        driver.close();
+    }
 
 }
